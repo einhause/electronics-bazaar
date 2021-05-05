333d034489981887fac0784715801a804350c07a
Eric Einhaus <eeinhaus17@gmail.com>
2021-05-05 19:11:02 -0400

    customer error handling middleware, returns formatted errors in JSON

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -22,8 +22,12 @@ router.get(
   '/:id',
   asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
-    if (!product) return res.status(404).json({ msg: 'Product not found' });
-    return res.json(product);
+    if (!product) {
+      res.status(404);
+      throw new Error('Product not found');
+    } else {
+      res.json(product);
+    }
   })
 );
 
