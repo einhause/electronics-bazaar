306aa914fe6593626c356230a4963dad337d1862
Eric Einhaus <eeinhaus17@gmail.com>
2021-05-06 18:07:26 -0400

    Add to Cart functionality

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants';
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
-  const { data } = axios.get(`/api/products/${id}`);
+  const { data } = await axios.get(`/api/products/${id}`);
   const { _id, name, image, price, countInStock } = data;
 
   dispatch({
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link, useParams, useHistory, useLocation } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Buttom,
+  Card,
+} from 'react-bootstrap';
+import Message from '../components/Message';
+import { addToCart } from '../actions/cartActions';
 
 const CartScreen = () => {
+  const { id: productId } = useParams();
+  const history = useHistory();
+  //const location = useLocation();
+  //const qty = location.search ? Number(location.search.split('=')[1]) : 1;
+
+  const dispatch = useDispatch();
+
+  const cartItems = useSelector((state) => state.cart.cartItems);
+  const { _id, name, image, price, countInStock, qty } = cartItems;
+
+  useEffect(() => {
+    if (productId) dispatch(addToCart(productId, qty));
+  }, [dispatch, productId, qty]);
+
   return <div>Cart</div>;
 };
 
