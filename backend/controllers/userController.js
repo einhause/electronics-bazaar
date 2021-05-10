40c03aeb14d760160870e4337a524813f8b9db7e
Eric Einhaus <eeinhaus17@gmail.com>
2021-05-10 15:34:20 -0400

    Registeration functionality in the backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,4 @@
+import e from 'express';
 import asyncHandler from 'express-async-handler'; //middleware for async error handling, avoid trycatching
 import User from '../models/userModel.js';
 import generateToken from '../utils/generateToken.js';
@@ -24,6 +25,39 @@ const authUser = asyncHandler(async (req, res) => {
   throw new Error('Invalid credentials!');
 });
 
+// @desc Register a new user
+// @route POST /api/users
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid User Data');
+  }
+});
+
 // @desc Get user profile
 // @route GET /api/users/profile
 // @access Private
@@ -43,4 +77,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile };
+export { authUser, registerUser, getUserProfile };
