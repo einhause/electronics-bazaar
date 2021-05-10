170e22615ad415c2b960e2ba95d454d388bb8cd2
Eric Einhaus <eeinhaus17@gmail.com>
2021-05-10 15:50:16 -0400

    User Login constant reducer, and action

---
+++ frontend/src/reducers/userReducers.js
@@ -0,0 +1,22 @@
+import {
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGIN_FAIL,
+  USER_LOGOUT,
+} from '../constants/userConstants';
+
+export const userLoginReducer = (state = {}, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case USER_LOGIN_REQUEST:
+      return { ...state, loading: true };
+    case USER_LOGIN_SUCCESS:
+      return { ...state, loading: false, userInfo: payload };
+    case USER_LOGIN_FAIL:
+      return { ...state, loading: false, error: payload };
+    case USER_LOGOUT:
+      return {};
+    default:
+      return state;
+  }
+};
