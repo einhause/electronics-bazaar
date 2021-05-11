38753930e08cf91b5f5e74f32e7394e886b2d1bb
Eric Einhaus <eeinhaus17@gmail.com>
2021-05-10 21:14:01 -0400

    User registration reducer and actions

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -3,6 +3,9 @@ import {
   USER_LOGIN_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGOUT,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL,
 } from '../constants/userConstants';
 
 export const userLoginReducer = (state = {}, action) => {
@@ -16,6 +19,20 @@ export const userLoginReducer = (state = {}, action) => {
       return { ...state, loading: false, error: payload };
     case USER_LOGOUT:
       return {};
+    default:
+      return state;
+  }
+};
+
+export const userRegisterReducer = (state = {}, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case USER_REGISTER_REQUEST:
+      return { ...state, loading: true };
+    case USER_REGISTER_SUCCESS:
+      return { ...state, loading: false, userInfo: payload };
+    case USER_REGISTER_FAIL:
+      return { ...state, loading: false, error: payload };
     default:
       return state;
   }
