3746314de7410d57bab915439ff28fff48b2c3fb
Eric Einhaus <eeinhaus17@gmail.com>
2021-05-13 08:36:36 -0400

    Payment method screen, action, and reducer

---
+++ frontend/src/screens/PaymentScreen.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import {
+  Form,
+  Button,
+  Col,
+  FormGroup,
+  FormLabel,
+  FormCheck,
+} from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import FormContainer from '../components/FormContainer';
+import CheckoutSteps from '../components/CheckoutSteps';
+import { savePaymentMethod } from '../actions/cartActions';
+
+const PaymentScreen = () => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const { shippingAddress } = useSelector((state) => state.cart);
+
+  if (!shippingAddress) history.push('/shipping');
+
+  const [paymentMethod, setPaymentMethod] = useState('PayPal');
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(savePaymentMethod(paymentMethod));
+    history.push('/placeorder');
+  };
+
+  return (
+    <FormContainer>
+      <CheckoutSteps step1 step2 step3 />
+      <h1>Payment Method</h1>
+      <Form onSubmit={submitHandler}>
+        <FormGroup>
+          <FormLabel as='legend'>Select Method</FormLabel>
+
+          <Col>
+            <FormCheck
+              type='radio'
+              label='PayPal or Credit Card'
+              id='PayPal'
+              name='paymentMethod'
+              value='PayPal'
+              onChange={(e) => setPaymentMethod(e.target.value)}
+              checked
+            ></FormCheck>
+            <FormCheck
+              type='radio'
+              label='Stripe'
+              id='Stripe'
+              name='paymentMethod'
+              value='Stripe'
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></FormCheck>
+          </Col>
+        </FormGroup>
+
+        <Button type='submit' variant='primary'>
+          Continue
+        </Button>
+      </Form>
+    </FormContainer>
+  );
+};
+
+export default PaymentScreen;
